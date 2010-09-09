c72a51882e624d97d831b96e3e3750dc07c25145
Branko Vukelic <bg.branko@gmail.com>
2010-09-09 20:44:12 +0200

    Working version with datadump

---
+++ console.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    // Load credentials from cookie (if any)
+    var creds = $.cookie("rdbconsole_creds");
+    if (creds) {
+        $("#username").val(creds.slice(0, 11));
+        $("#authcode").val(creds.slice(11, 61));
+    }
+    
+    $("#query_form").submit(function (event) {
+        event.preventDefault();
+    });
+    
+    $("#remove_cred").click(function () {
+        $("#username").val("username");
+        $("#authcode").val("authcode");
+        $.cookie("rdbconsole_creds", null);
+    });
+    
+    // Submit handler
+    $("#query_form").validate({
+        submitHandler: function (form) {
+            $("#console").empty();
+            var username = $("#username").val();
+            var authcode = $("#authcode").val();
+            $.cookie("rdbconsole_creds", username + authcode, {expires: 20});
+            $("#console").append('<img src="loading.gif" alt="Loading..." />');
+            $.rdbHostConfig({
+                userName: username,
+                authcode: authcode,
+                errback: function (error_data) {
+                    $("#console").html("<strong>Error:</strong> " + error_data);
+                }
+            })
+            $("#console").datadump({
+                q: $("#query").val()
+            });
+        }
+    })
+});
